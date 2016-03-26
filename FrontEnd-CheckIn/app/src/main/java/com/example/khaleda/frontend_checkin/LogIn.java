a336aa085ce8be20667159c8661b391ce2fa0a84
Ali Abdelrahman <aliabdelrahmanweka74@gmail.com>
2016-03-26 16:28:09 +0200

    Log in Works

--- FrontEnd-CheckIn/app/src/main/java/com/example/khaleda/frontend_checkin/LogIn.java
+++ FrontEnd-CheckIn/app/src/main/java/com/example/khaleda/frontend_checkin/LogIn.java
@@ -10,12 +10,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LogIn extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogIn extends AppCompatActivity {
+    static RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
     }
 
     @Override
@@ -36,14 +52,34 @@ public class LogIn extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
     public void logIn(View view) {
-        EditText email = (EditText)findViewById(R.id.email_log_in);
-        EditText password = (EditText)findViewById(R.id.password_log_in);
-        String EmailText = email.getText().toString();
-        String PasswordText = password.getText().toString();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                "http://firstapp-seapp.rhcloud.com/FCISquareBackend-master-1cb83b45ba096969d4bc7b9c7baa6a12ba4c1227/rest/login",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(LogIn.this, response , Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LogIn.this, error.getMessage() + "AAAA", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> loginData = new HashMap<String, String>();
+                loginData.put("email" ,((EditText)findViewById(R.id.email_log_in)).getText().toString());
+                loginData.put("pass",((EditText)findViewById(R.id.password_log_in)).getText().toString());
+                return loginData;
+
+            }
+        };
+        LogIn.requestQueue.add(stringRequest);
     }
 
 }
